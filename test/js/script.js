9fec88a3de52fd301273decddd1e49daaca71dcf
SoftwareDev15 <jarunya.15th@gmail.com>
2023-06-28 15:00:47 +0700

    success index.html

--- test/js/script.js
+++ test/js/script.js
@@ -2,7 +2,7 @@ let menu = document.querySelector("#menu");
 let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-  menu.classList.toggle("fa-times");
+  // menu.classList.toggle("fa-times");
   navbar.classList.toggle("active");
 };
 
